9e033f9875b51112408b683bbfe00cb5d705076e
kabadasori <kabe.soresa@gmail.com>
2023-06-12 04:49:44 -0700

    python data structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+# Bedasa Wayessa <bedonaf@gmail.com>
+
+
 def print_list_integer(my_list=[]):
-        for i in my_list:
-                print("{:d}".format(i))
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
